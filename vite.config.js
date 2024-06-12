df7a9c8ef018dee1ab32f6fcdc7fd82118905f3d
DeeCode <111423769+DeeCode2@users.noreply.github.com>
2024-06-12 10:09:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://deecode2.github.io/LittleCorner/"
+  base: "https://deecode2.github.io/LittleCorner/",
   test: {
     globals: true,
     environment: 'jsdom',
